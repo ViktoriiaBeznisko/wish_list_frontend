a055022a3aa0b851163abc6ea369472d3f1b2a2b
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2021-02-03 17:00:35 -0500

    wish_form.js updated

--- src/components/wish/WishForm.js
+++ src/components/wish/WishForm.js
@@ -1,15 +1,12 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux'
 // import { submitWishList, wishListChange, setWishLists, unsetWishList, setSelectedWishlists, updateWishList, deleteWishList, submitWish, wishChange, setWishes, setSelectedWish, updateWish, deleteWish, unsetWish } from '../../redux/actionCreators'
-
+import {Field,reduxForm} from "redux-form";
+import classes from './wish_form.module.css';
 
 function WishForm(props) {
-
-  // console.log(props.edit)
-  // update value, 'props.edit ?.... - saved previos state (name) of wish
   const [input1, setInput1] = useState(props.edit ? props.edit.value1 : '');
   const [input2, setInput2] = useState(props.edit ? props.edit.value2 : '');
-
   const handleChange = e => {
     if (e.target.name === "text"){
       setInput1(e.target.value);
@@ -32,55 +29,66 @@ function WishForm(props) {
     setInput1('');
     setInput2('');
   };
-
   return (
-    <form onSubmit={handleSubmit} className='wish-form'>
+    <form onSubmit={handleSubmit} className={classes.main}>
+      <h2 className={classes.title}>{props.edit ? 'Change Wish Name:' : 'Add wish:'}</h2>
       {props.edit ? (
-        <>
-          <input
-            placeholder='Update your wish'
-            value={input1}
-            onChange={handleChange}
-            name='text'
-            className='wish-input edit'
-          />
-          <input
-            placeholder='Update link'
-            value={input2}
-            onChange={handleChange}
-            name='link'
-            className='link-input edit'
-          />
-          <button onClick={handleSubmit} className='wish-button edit'>
-            Update
-          </button>
-        </>
+
+          <div className={classes.addBlock}>
+            <div className={classes.row1}>
+              <input
+                  placeholder='Update your wish'
+                  value={input1}
+                  onChange={handleChange}
+                  name='text'
+                  className={classes.input}
+                  autoComplete={'off'}
+              />
+              <input
+                  placeholder='Update link'
+                  value={input2}
+                  onChange={handleChange}
+                  name='link'
+                  className={classes.input}
+                  autoComplete={'off'}
+              /></div>
+            <div className={classes.row2}>
+              <button onClick={handleSubmit} className={classes.button}>
+                Update
+              </button>
+            </div>
+          </div>
       ) : (
-        <>
+        <div className={classes.addBlock}>
+          <div className={classes.row1}>
           <input
             placeholder='Add a wish'
             value={input1}
             onChange={handleChange}
             name='text'
-            className='wish-input'
+            className={classes.input}
+            autoComplete={'off'}
           />
           <input
             placeholder='Add a link'
             value={input2}
             onChange={handleChange}
             name='link'
-            className='link-input'
-          />
-          <button onClick={handleSubmit} className='wish-button'>
+            className={classes.input}
+            autoComplete={'off'}
+          /></div>
+          <div className={classes.row2}>
+          <button onClick={handleSubmit} className={classes.button}>
             Add wish
           </button>
-        </>
+          </div>
+        </div>
+
+
       )}
     </form>
   );
 }
-
-
 
 
 // export default connect(mapStateToProps, { submitWishList, wishListChange, setWishLists, unsetWishList, setSelectedWishlists, updateWishList, deleteWishList, submitWish, wishChange, setWishes, setSelectedWish, updateWish, deleteWish, unsetWish })(WishForm)
